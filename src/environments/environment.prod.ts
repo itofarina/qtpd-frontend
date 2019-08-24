4a3850295dfa3131313d11e94a426531ad79761c
Alvaro Farina <farinalvaro@gmail.com>
2019-08-24 13:03:40 -0300

    updates to upload to heroku

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    APIEndpoint: $ENV.APIEndpoint,
+    environment: $ENV.ENVIRONMENT,
+    googleAnalyticsKey: 'UA-61396425-1',
+    googleMapsApiKey: $ENV.googleMapsApiKey
+};
